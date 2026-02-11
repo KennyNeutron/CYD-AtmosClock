8474757cf920c66fdeeb76cbc1c45788d5ca7faf
KennyNeutron <kennyneutron.hex@gmail.com>
2026-02-12 02:17:30 +0800

    feat: initialize project with LVGL, TFT_eSPI display, and XPT2046 touchscreen support

--- firmware/CYD_AtmosClock/CYD_AtmosClock.ino
+++ firmware/CYD_AtmosClock/CYD_AtmosClock.ino
@@ -59,79 +59,31 @@ void touchscreen_read(lv_indev_t * indev, lv_indev_data_t * data) {
   }
 }
 
-int btn1_count = 0;
-// Callback that is triggered when btn1 is clicked
-static void event_handler_btn1(lv_event_t * e) {
-  lv_event_code_t code = lv_event_get_code(e);
-  if(code == LV_EVENT_CLICKED) {
-    btn1_count++;
-    LV_LOG_USER("Button clicked %d", (int)btn1_count);
-  }
-}
-
-// Callback that is triggered when btn2 is clicked/toggled
-static void event_handler_btn2(lv_event_t * e) {
-  lv_event_code_t code = lv_event_get_code(e);
-  lv_obj_t * obj = (lv_obj_t*) lv_event_get_target(e);
-  if(code == LV_EVENT_VALUE_CHANGED) {
-    LV_UNUSED(obj);
-    LV_LOG_USER("Toggled %s", lv_obj_has_state(obj, LV_STATE_CHECKED) ? "on" : "off");
-  }
-}
-
-static lv_obj_t * slider_label;
-// Callback that prints the current slider value on the TFT display and Serial Monitor for debugging purposes
-static void slider_event_callback(lv_event_t * e) {
-  lv_obj_t * slider = (lv_obj_t*) lv_event_get_target(e);
-  char buf[8];
-  lv_snprintf(buf, sizeof(buf), "%d%%", (int)lv_slider_get_value(slider));
-  lv_label_set_text(slider_label, buf);
-  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-  LV_LOG_USER("Slider changed to %d%%", (int)lv_slider_get_value(slider));
-}
-
 void lv_create_main_gui(void) {
-  // Create a text label aligned center on top ("Hello, world!")
+  // Set the background color of the active screen to black
+  lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x000000), LV_PART_MAIN);
+
+  // Create a text label aligned center
   lv_obj_t * text_label = lv_label_create(lv_screen_active());
   lv_label_set_long_mode(text_label, LV_LABEL_LONG_WRAP);    // Breaks the long lines
-  lv_label_set_text(text_label, "Hello World! LVGL by Kenny Neutron");
+  lv_label_set_text(text_label, "ESP32 CYD AtmosClock");
   lv_obj_set_width(text_label, 340);    // Set smaller width to make the lines wrap
   lv_obj_set_style_text_align(text_label, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_align(text_label, LV_ALIGN_CENTER, 0, -100);
+  lv_obj_align(text_label, LV_ALIGN_CENTER, 0, 0);
 
-  lv_obj_t * btn_label;
-  // Create a Button (btn1)
-  lv_obj_t * btn1 = lv_button_create(lv_screen_active());
-  lv_obj_add_event_cb(btn1, event_handler_btn1, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -50);
-  lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
-
-  btn_label = lv_label_create(btn1);
-  lv_label_set_text(btn_label, "Button");
-  lv_obj_center(btn_label);
-
-  // Create a Toggle button (btn2)
-  lv_obj_t * btn2 = lv_button_create(lv_screen_active());
-  lv_obj_add_event_cb(btn2, event_handler_btn2, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 10);
-  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-  btn_label = lv_label_create(btn2);
-  lv_label_set_text(btn_label, "Toggle");
-  lv_obj_center(btn_label);
-  
-  // Create a slider aligned in the center bottom of the TFT display
-  lv_obj_t * slider = lv_slider_create(lv_screen_active());
-  lv_obj_align(slider, LV_ALIGN_CENTER, 0, 60);
-  lv_obj_add_event_cb(slider, slider_event_callback, LV_EVENT_VALUE_CHANGED, NULL);
-  lv_slider_set_range(slider, 0, 100);
-  lv_obj_set_style_anim_duration(slider, 2000, 0);
-
-  // Create a label below the slider to display the current slider value
-  slider_label = lv_label_create(lv_screen_active());
-  lv_label_set_text(slider_label, "0%");
-  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+  /*
+   * TO PLAY A GIF:
+   * 1. Convert your GIF file to a C array using the LVGL Online Converter: https://lvgl.io/tools/imageconverter
+   *    - Select "C array" as the output format.
+   *    - Select "CF_RAW" or "CF_RAW_ALPHA" (if it has transparency) for the color format.
+   * 2. Copy the generated C file content (or include the file) into your project.
+   * 3. Declare the image variable here:
+   *    LV_IMG_DECLARE(my_gif_variable_name);
+   * 4. Use the following code to display it:
+   *    lv_obj_t * img = lv_gif_create(lv_screen_active());
+   *    lv_gif_set_src(img, &my_gif_variable_name);
+   *    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+   */
 }
 
 void setup() {
